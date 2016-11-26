4f506a9d661f1bf2a8f38dc3f9659b60006092f3
Dan Geabunea <dan.geabunea@jlg.ro>
2016-11-26 13:35:56 +0200

    Finished SpringBoot Multiple Maven Projects Demo;

--- SpringBootMultipleMavenModules/web/src/main/java/rc/web/DemoApplication.java
+++ SpringBootMultipleMavenModules/web/src/main/java/rc/web/DemoApplication.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"rc.persistence"})
-@EntityScan(basePackages = {"rc.domain"})
-@ComponentScan(basePackages = {"rc.domain","rc.persistence","rc.web"})
+@EnableJpaRepositories(basePackages = {"rc"})
+@EntityScan(basePackages = {"rc"})
+@ComponentScan(basePackages = {"rc"})
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
