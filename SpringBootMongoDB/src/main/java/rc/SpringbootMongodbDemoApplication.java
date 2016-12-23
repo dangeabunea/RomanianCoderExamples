08139b5fa23f065e9b7c58dff1e0fc2137946452
Dan Geabunea <dan.geabunea@jlg.ro>
2016-12-23 10:08:18 +0200

    Added new project: SpringBootWithMongoDB

---
+++ SpringBootMongoDB/src/main/java/rc/SpringbootMongodbDemoApplication.java
@@ -0,0 +1,12 @@
+package rc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbDemoApplication.class, args);
+	}
+}
