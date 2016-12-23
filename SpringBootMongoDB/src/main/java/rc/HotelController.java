3313a322bfe6aa26884b780625421f267307348b
Dan Geabunea <dan.geabunea@jlg.ro>
2016-12-23 15:07:21 +0200

    [SpringBootMongoDB] Finished

--- SpringBootMongoDB/src/main/java/rc/HotelController.java
+++ SpringBootMongoDB/src/main/java/rc/HotelController.java
@@ -1,5 +1,6 @@
 package rc;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,10 +31,56 @@ public class HotelController {
         this.hotelRepository.save(hotel);
     }
 
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") String id){
-        this.hotelRepository.delete(id);
+    @GetMapping("/{id}")
+    public Hotel getById(@PathVariable("id") String id){
+        Hotel hotel = this.hotelRepository.findById(id);
+
+        return hotel;
     }
 
-    
+    @GetMapping("/price/{maxPrice}")
+    public List<Hotel> getByPricePerNight(@PathVariable("maxPrice") int maxPrice){
+        List<Hotel> hotels = this.hotelRepository.findByPricePerNightLessThan(maxPrice);
+
+        return hotels;
+    }
+
+    @GetMapping("/address/{city}")
+    public List<Hotel> getByCity(@PathVariable("city") String city){
+        List<Hotel> hotels = this.hotelRepository.findByCity(city);
+
+        return hotels;
+    }
+
+    @GetMapping("/country/{country}")
+    public List<Hotel> getByCountry(@PathVariable("country") String country){
+        // create a query class (QHotel)
+        QHotel qHotel = new QHotel("hotel");
+
+        // using the query class we can create the filters
+        BooleanExpression filterByCountry = qHotel.address.country.eq(country);
+
+        // we can then pass the filters to the findAll() method
+        List<Hotel> hotels = (List<Hotel>) this.hotelRepository.findAll(filterByCountry);
+
+        return hotels;
+    }
+
+    @GetMapping("/recommended")
+    public List<Hotel> getRecommended(){
+        final int maxPrice = 100;
+        final int minRating = 7;
+
+        // create a query class (QHotel)
+        QHotel qHotel = new QHotel("hotel");
+
+        // using the query class we can create the filters
+        BooleanExpression filterByPrice = qHotel.pricePerNight.lt(maxPrice);
+        BooleanExpression filterByRating = qHotel.reviews.any().rating.gt(minRating);
+
+        // we can then pass the filters to the findAll() method
+        List<Hotel> hotels = (List<Hotel>) this.hotelRepository.findAll(filterByPrice.and(filterByRating));
+
+        return hotels;
+    }
 }
--- SpringBootMongoDB/src/main/java/rc/HotelRepository.java
+++ SpringBootMongoDB/src/main/java/rc/HotelRepository.java
@@ -1,8 +1,17 @@
 package rc;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface HotelRepository extends MongoRepository<Hotel,String> {
+public interface HotelRepository extends MongoRepository<Hotel,String>, QueryDslPredicateExecutor<Hotel> {
+    Hotel findById(String id);
+    List<Hotel> findByPricePerNightLessThan(int maxPrice);
+
+    @Query(value = "{address.city:?0}")
+    List<Hotel> findByCity(String city);
 }
