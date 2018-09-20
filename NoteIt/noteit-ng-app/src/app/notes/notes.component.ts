b4cc09c9758026fed4e39cd0596fedcd475aaa3a
Dan Geabunea <dangeabunea@hotmail.com>
2018-09-20 19:39:40 +0300

    Episode 15 milestone 2

--- NoteIt/noteit-ng-app/src/app/notes/notes.component.ts
+++ NoteIt/noteit-ng-app/src/app/notes/notes.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Component, OnInit} from '@angular/core';
 import {Notebook} from "./model/notebook";
 import {ApiService} from "../shared/api.service";
 
@@ -11,13 +10,14 @@ import {ApiService} from "../shared/api.service";
 export class NotesComponent implements OnInit {
   notebooks: Notebook[] = [];
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {
+  }
 
   ngOnInit() {
     this.getAllNotebooks();
   }
 
-  public getAllNotebooks(){
+  public getAllNotebooks() {
     this.apiService.getAllNotebooks().subscribe(
       res => {
         this.notebooks = res;
@@ -26,5 +26,22 @@ export class NotesComponent implements OnInit {
         alert("An error has occurred;")
       }
     );
+  }
+
+  createNotebook() {
+    let newNotebook:Notebook = {
+      name:'New notebook',
+      id: null,
+      nbOfNotes: 0
+    };
+
+    this.apiService.postNotebook(newNotebook).subscribe(
+      res => {
+        newNotebook.id = res.id;
+        this.notebooks.push(newNotebook);
+      },
+      err => {alert("An error has occurred while saving the notebook");}
+    );
+
   }
 }
--- NoteIt/noteit-ng-app/src/app/shared/api.service.ts
+++ NoteIt/noteit-ng-app/src/app/shared/api.service.ts
@@ -11,6 +11,7 @@ export class ApiService {
   private BASE_URL = "http://localhost:8082/api";
   private ALL_NOTEBOOKS_URL = `${this.BASE_URL}\\notebooks\\all`;
   private SEND_FEEDBACK_URL  = `${this.BASE_URL}\\feedback`;
+  private SAVE_UPDATE_NOTEBOOK = `${this.BASE_URL}\\notebooks`;
 
   constructor(private http: HttpClient) {
 
@@ -22,5 +23,9 @@ export class ApiService {
 
   postFeedback(feedback: FeedbackViewModel) : Observable<any> {
     return this.http.post(this.SEND_FEEDBACK_URL, feedback);
+  }
+
+  postNotebook(notebook: Notebook): Observable<Notebook>{
+    return this.http.post<Notebook>(this.SAVE_UPDATE_NOTEBOOK, notebook);
   }
 }
