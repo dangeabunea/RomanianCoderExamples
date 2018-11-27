82b5f3cd3e90f8da6ce17b85a73fe5deeaf3dd50
Dan Geabunea <dangeabunea@hotmail.com>
2018-11-27 10:13:08 +0200

    SpringBoot Security Course Init

---
+++ SpringBootSecurity/UnsecureExample/src/main/java/rc/course/springbootsecurity/SpringbootsecurityApplication.java
@@ -0,0 +1,12 @@
+package rc.course.springbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsecurityApplication.class, args);
+    }
+}
