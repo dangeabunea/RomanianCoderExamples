8d0036d3e5befb2aa68ada8b91ea009e6609b7d6
Dan Geabunea <dangeabunea@hotmail.com>
2018-12-05 11:11:46 +0200

    Role based security;

--- SpringBootSecurity/HttpBasic/src/main/java/rc/bootsecurity/SecurityConfiguration.java
+++ SpringBootSecurity/HttpBasic/src/main/java/rc/bootsecurity/SecurityConfiguration.java
@@ -19,14 +19,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
                 .and()
-                .withUser("dan").password(passwordEncoder().encode("dan123")).roles("USER");
+                .withUser("dan").password(passwordEncoder().encode("dan123")).roles("USER")
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("ADMIN","MANAGER")
+                .antMatchers("/api/public/**").authenticated()
                 .and()
                 .httpBasic();
     }
