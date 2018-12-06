11e8deed7178ba7f727ecb1d0b3ccecc9be40530
Dan Geabunea <dangeabunea@hotmail.com>
2018-12-06 09:25:54 +0200

    Added SSL capability

--- SpringBootSecurity/HttpBasic/src/main/java/rc/bootsecurity/SecurityConfiguration.java
+++ SpringBootSecurity/HttpBasic/src/main/java/rc/bootsecurity/SecurityConfiguration.java
@@ -17,11 +17,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN").authorities("ACCESS_TEST1", "ACCESS_TEST2")
                 .and()
-                .withUser("dan").password(passwordEncoder().encode("dan123")).roles("USER")
+                .withUser("dan")
+                .password(passwordEncoder().encode("dan123"))
+                .roles("USER")
                 .and()
-                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
+                .withUser("manager")
+                .password(passwordEncoder().encode("manager123"))
+                .roles("MANAGER").authorities("ACCESS_TEST1");
     }
 
     @Override
@@ -31,8 +37,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/index.html").permitAll()
                 .antMatchers("/profile/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/management/**").hasAnyRole("ADMIN","MANAGER")
-                .antMatchers("/api/public/**").authenticated()
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
                 .and()
                 .httpBasic();
     }
