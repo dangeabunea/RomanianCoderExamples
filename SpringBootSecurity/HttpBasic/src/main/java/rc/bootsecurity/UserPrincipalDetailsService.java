ee7b99f2a318cc380f4685c3fbdbcbb346be28f2
Dan Geabunea <dangeabunea@hotmail.com>
2018-12-23 13:56:19 +0200

    Added impl for UserDetails and UserDetailsService;

---
+++ SpringBootSecurity/HttpBasic/src/main/java/rc/bootsecurity/UserPrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package rc.bootsecurity;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import rc.bootsecurity.db.UserRepository;
+import rc.bootsecurity.model.User;
+
+public class UserPrincipalDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(s);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+}
