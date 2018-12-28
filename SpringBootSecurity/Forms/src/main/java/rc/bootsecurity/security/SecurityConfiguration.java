eca55ccca0cbc13df93f79f3a3054bb84132331b
Dan Geabunea <dangeabunea@hotmail.com>
2018-12-28 18:05:16 +0200

    Episode 24 - Remember me feature

--- SpringBootSecurity/Forms/src/main/java/rc/bootsecurity/security/SecurityConfiguration.java
+++ SpringBootSecurity/Forms/src/main/java/rc/bootsecurity/security/SecurityConfiguration.java
@@ -40,7 +40,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+                .and()
+                .rememberMe().tokenValiditySeconds(2592000).key("mySecret!");
     }
 
     @Bean
