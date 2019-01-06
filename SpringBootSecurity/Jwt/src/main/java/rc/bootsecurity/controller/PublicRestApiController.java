753cbb5b9abb05b25562f79d0457b6a05d40f75a
Dan Geabunea <dangeabunea@hotmail.com>
2019-01-06 17:36:02 +0200

    Episode 27 - Added JWT dependencies;

--- SpringBootSecurity/Jwt/src/main/java/rc/bootsecurity/controller/PublicRestApiController.java
+++ SpringBootSecurity/Jwt/src/main/java/rc/bootsecurity/controller/PublicRestApiController.java
@@ -1,5 +1,6 @@
 package rc.bootsecurity.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/public")
+@CrossOrigin
 public class PublicRestApiController {
     private UserRepository userRepository;
 
