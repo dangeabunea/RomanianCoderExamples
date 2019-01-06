6433867300d703b4013064294f36b21927c67a35
Dan Geabunea <dangeabunea@hotmail.com>
2019-01-06 15:22:38 +0200

    Episode 27 - Added base JWT project template;

---
+++ SpringBootSecurity/Jwt/src/main/java/rc/bootsecurity/controller/PublicRestApiController.java
@@ -0,0 +1,37 @@
+package rc.bootsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rc.bootsecurity.db.UserRepository;
+import rc.bootsecurity.model.User;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/public")
+public class PublicRestApiController {
+    private UserRepository userRepository;
+
+    public PublicRestApiController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    // Available to all authenticated users
+    @GetMapping("test")
+    public String test1(){
+        return "API Test";
+    }
+
+    // Available to managers
+    @GetMapping("management/reports")
+    public String test2(){
+        return "Some report data";
+    }
+
+    // Available to ROLE_ADMIN
+    @GetMapping("admin/users")
+    public List<User> users(){
+        return this.userRepository.findAll();
+    }
+}
---
+++ SpringBootSecurity/Jwt/src/main/java/rc/bootsecurity/security/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package rc.bootsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private UserPrincipalDetailsService userPrincipalDetailsService;
+
+    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService) {
+        this.userPrincipalDetailsService = userPrincipalDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("*").permitAll();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.userPrincipalDetailsService);
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
