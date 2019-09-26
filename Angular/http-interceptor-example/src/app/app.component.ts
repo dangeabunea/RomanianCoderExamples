d87401057619fa94344a5f9543135c11daf4821a
Dan Geabunea <dangeabunea@hotmail.com>
2019-09-26 16:12:06 +0300

    Added blank http interceptor project

---
+++ Angular/http-interceptor-example/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'http-interceptor-example';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+
+    // Executing 3 HTTP Requests
+
+    this.http.get('http://jsonplaceholder.typicode.com/users')
+      .subscribe(data => {
+        console.log(data);
+      });
+
+    this.http.get('http://jsonplaceholder.typicode.com/posts/2')
+      .subscribe(data => {
+        console.log(data);
+      });
+
+    this.http.get('http://jsonplaceholder.typicode.com/posts/2/comments')
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+}
